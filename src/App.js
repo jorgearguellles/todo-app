a51cd64d3cf7cb2c81f7154cb2f12885f45ca41d
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-11-08 22:05:31 +0100

    add/ data persistace by localStorage. Next create useLocalStorage custom hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import { useState } from "react";
 import AppUI from "./pages/AppUI";
 
-const defaultTodos = [
-  {text: "jorge", completed: true},
-  {text: "alberto", completed: false},
-  {text: "arias", completed: false},
-  {text: "argüelles", completed: false}
-];
+// const defaultTodos = [
+//   {text: "jorge", completed: true},
+//   {text: "alberto", completed: false},
+//   {text: "arias", completed: false},
+//   {text: "argüelles", completed: false}
+// ];
+
+function useLocalStorage( itemName, initialValue ){
+
+  // Persistencia de datos usando el LocalStorage. JSON.parse & JSON.stringify
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if(!localStorageItem){
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  };
+
+  const [item, setItem] = useState(parsedItem);
+
+  // Persistencia de datos vinculando la funcionalidad de marcar como hecho y eliminat todo
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [
+    item,
+    saveItem
+  ];
+};
+
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V3', []);
+
   const [searchValue, setSearchValue] = useState("");
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -26,21 +56,23 @@ function App() {
     })
   }
 
-// Método para marcar item como completado
-const completeTodo = (text) => {
-  const todoIndex = todos.findIndex(todo => todo.text === text);
-  const newTodos = [...todos];
-  newTodos[todoIndex].completed = true;
-  setTodos(newTodos);
-};
+  
 
-// Método para eliminar item 
-const deleteTodo = (text) => {
-  const todoIndex = todos.findIndex(todo => todo.text === text);
-  const newTodos = [...todos];
-  newTodos.splice(todoIndex,1);
-  setTodos(newTodos);
-};
+  // Método para marcar item como completado
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
+  // Método para eliminar item 
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex,1);
+    saveTodos(newTodos);
+  };
 
   return (
     <AppUI 
